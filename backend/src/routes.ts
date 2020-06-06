8ab52070d68560e4a6616c036456f5b26cf92eef
Ivan Costa <ivan.costa@justa.com.vc>
2020-06-06 18:47:46 -0300

    feat(backend): Adiciona backend da aplicacao feito em nodejs;
    Signed-off-by: Ivan Costa <ivan.costa@justa.com.vc>

---
+++ backend/src/routes.ts
@@ -0,0 +1,24 @@
+
+import express, { request, response } from 'express';
+
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
+
+// index, show, create, update, delete
+
+const routes = express.Router();
+const itemsController = new ItemsController();
+const pointController = new PointsController();
+ 
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointController.create);
+routes.get('/points', pointController.index);
+routes.get('/points/:id', pointController.show);
+ 
+
+export default routes;
+
+//TO-DO
+//Service Pattern
+//Repository Pattern  || Data Mapper
